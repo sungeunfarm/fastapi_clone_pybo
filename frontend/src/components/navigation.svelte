e460a6bf97d8ac1cb27ed2e2ec60557282a4b75c
corinicode <corincode@gmail.com>
2022-11-03 02:57:49 +0900

    네비게이션바,페이징,스토어,날짜,게시물 일련번호,답변개수표시

---
+++ frontend/src/components/navigation.svelte
@@ -0,0 +1,24 @@
+<script>
+    import {link} from 'svelte-spa-router'
+    import {page} from '../lib/store'
+</script>
+
+<!-- 네비게이션 -->
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+    <div class="container-fluid">
+        <a href="/" class="navbar-brand" use:link on:click="{()=>{$page = 0}}">Pybo</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a use:link href="/user-create" class="nav-link">회원가입</a>
+                </li>
+                <li class="nav-item">
+                    <a use:link class="nav-link" href="/user-login">로그인</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- frontend/src/routes/Detail.svelte
+++ frontend/src/routes/Detail.svelte
@@ -1,6 +1,9 @@
 <script>
     import fastapi from "../lib/api";
     import Error from "../components/Error.svelte";
+    import {push} from 'svelte-spa-router'
+    import moment from 'moment/min/moment-with-locales'
+    moment.locale('ko')
 
     export let params = {}
     let question_id = params.question_id
@@ -43,10 +46,12 @@ function post_answer(event) {
                 {question.content}
             </div>
             <div class="d-flex justify-content-end">
-                <div class="badge bg-light text-dark p-2">{question.create_date}</div>
+                <div class="badge bg-light text-dark p-2">{moment(question.create_date).format("YYYY년 MM월 DD일 hh:mm a")}</div>
             </div>
         </div>
     </div>
+    <button class="btn btn-secondary" on:click="{()=>{push('/')}}">목록으로</button>
+
     <!-- 답변목록 -->
     <h5 class="border-bottom my-3 py-2">{question.answers.length}개의 답변이 있습니다.</h5>
     {#each question.answers as answer}
@@ -56,7 +61,7 @@ function post_answer(event) {
                     {answer.content}
                 </div>
                 <div class="d-flex justify-content-end">
-                    <div class="badge bg-light text-dark p-2">{answer.create_date}</div>
+                    <div class="badge bg-light text-dark p-2">{moment(answer.create_date).format("YYYY년 MM월 DD일 hh:mm a")}</div>
                 </div>
             </div>
         </div>
